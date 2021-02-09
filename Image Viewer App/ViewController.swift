e006d01445f95d47408a565d05b0653784219151
eszter szabó <eszticity@gmail.com>
2021-02-09 13:27:36 +0100

    project created

---
+++ Image Viewer App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Viewer App
+//
+//  Created by Eszter Szabó on 2021. 02. 09..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
