369bc7be66a01f27a69dca10ce61cf89d7808f5d
eszter szabó <eszticity@gmail.com>
2021-02-10 19:23:02 +0100

    detailsViewController for binding added

---
+++ Image Viewer App/View/Controller/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  Pods
+//
+//  Created by Eszter Szabó on 2021. 02. 10..
+//
+
+import Foundation
+import UIKit
+ 
+class DetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
